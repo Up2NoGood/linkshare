a4ef2a91c2aa1dea156d818fee44a9beb9846b4c
Koldo <koldo@zoho.com>
2014-09-11 22:33:58 +0100

    adding routes for different pages in main menu

--- controllers/home.rb
+++ controllers/home.rb
@@ -3,4 +3,20 @@ class HomeController < LinkShare
   get '/' do
     erb :linkshare
   end
+
+  get '/public' do
+    erb :public
+  end
+
+  get '/personal' do
+    erb :personal
+  end
+
+  get '/shared' do
+    erb :shared
+  end
+
+  get '/friends' do
+    erb :friends
+  end
 end
